b0af4f1ba7941e65bed4a477479dedc3a70b7505
Khoa Nguyen <57794145+DangKhoa253@users.noreply.github.com>
2020-01-18 08:48:16 +0700

    Add files via upload
    Create a game which allows players guess a number in range 1,300. Each player has 5 times for this game: 
    + If right, ask player wanna play again? If yes, return the game for player
    + If wrong and over 5 times, ask player wanna play again?
    Notify player each turn they're right or wrong.

---
+++ Case Draft.py
@@ -0,0 +1,48 @@
+#let on game 
+import random as rd 
+
+#pick number:
+def begin():
+    x=int(input("Let pick random number: "))
+    if x in range(1,301):
+        print(x)
+    else:
+        print("Select another number, the accepted number in range 1,300: ")
+        return begin()
+# Play game 
+
+def result():
+        y=rd.randrange(1,301)
+        return y 
+def setup(): 
+    num=result()
+    print("The result is: ",num)
+    i=0  
+    while i < 5:
+        x=begin()
+        if x == num:
+            print("Congrats, the result is:", num, "at ", i + 1, "time(s)" )
+            input1 = input("Do you want to play again? (Yes/No) ")
+            if input1 == "Yes":
+                return setup()
+            else:
+                print(" Thanks for joining our game")
+                return  
+        else:
+            print("Huh, You're wrong", i + 1  ,"time(s).", "Let pick another number")
+            i +=1
+            continue
+    if i+1 >= 5:
+        print(" You lose this game, do you wanna play one more times? Yes/No")
+        input2= input()
+        if input2 == "Yes":
+            return setup()
+        else:
+            print("Thanks for joining our game")
+
+        
+
+setup()
+            
+            
+
