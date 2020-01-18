73bfa7884cbebf52759e52f0ee99f33bdc4f3d90
Khoa Nguyen <57794145+DangKhoa253@users.noreply.github.com>
2020-01-18 09:49:25 +0700

    Add files via upload
    Time countdown - use time library

---
+++ new focus time.py
@@ -0,0 +1,17 @@
+import time
+import os
+hour = int(input('Enter any amount of hours you want -+==> '))
+minute = int(input('Enter any amount of minutes you want -+==> '))
+second = int(input('Enter any amount of seconds you want -+==> '))
+time = hour*10800 + minute*3600 + second*60
+print('{}:{}:{}'.format(hour,minute,second))
+while time > 0:
+   time = time - 1
+   seconds = (time // 60) % 60
+   minutes = (time // 3600)
+   hours = (time // 10800)
+   print('Time Left -+==> ',hours,':',minutes,':',seconds,)
+   os.system("CLS")
+if time == 0:
+   print('Time Is Over!')
+
